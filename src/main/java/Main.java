01cf89e79f265bb419b924852cf26d47ea4e149a
iamkishan08 <kishanoffcl08@gmail.com>
2025-02-27 12:04:45 +0530

    Echo Command Implementaion

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
-       
+
+       Scanner scanner = new Scanner(System.in);
        while(true){
         System.out.print("$ ");
-        
-        Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         if(input.equals("exit 0")){
+            scanner.close();
             break;
         }
-        System.out.println(input + ": command not found");
+        input.trim();
+        String ec = input.substring(0,4);
+        
+        if(ec.equals("echo")){
+            System.out.println(input.substring(5));
+        }else{
+           System.out.println(input + ": command not found");
+        }
        }
+
+       
     }
 }
