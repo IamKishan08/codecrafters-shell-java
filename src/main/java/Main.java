6b715b8cb12aa8c5aa0b91ed6e2ed38a32f4ad5a
iamkishan08 <kishanoffcl08@gmail.com>
2025-02-27 11:47:16 +0530

    Exit BuiltIn

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,9 @@ public class Main {
         
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        if(input.equals("exit 0")){
+            break;
+        }
         System.out.println(input + ": command not found");
        }
     }
